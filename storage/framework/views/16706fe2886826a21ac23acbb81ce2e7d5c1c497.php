7c28ca75408aa0b9ba6a081df9eeda2d9425f7f1
Truong Xuan <truongxuan92vt@gmail.com>
2020-03-18 22:52:13 +0700

    update Admin Product

--- storage/framework/views/16706fe2886826a21ac23acbb81ce2e7d5c1c497.php
+++
@@ -1,8 +0,0 @@
-<div style="background-color: #0b93d5">
-    <div class="container">
-        <span><b>Copyright © 2016-2018 by <a style="text-decoration: none; color: white">Truong Xuan</a></b>.</span>
-        <div class="pull-right">
-            <span><b>Version</b> 1.0.0</span>
-        </div>
-    </div>
-</div>
--- storage/framework/views/8a0af9a88bce2c68789f65b33fe4752598b860dd.php
+++
@@ -1,8 +0,0 @@
-
-<?php $__env->startSection('title', 'Dashboard'); ?>
-<?php $__env->startSection('controller', 'Dashboard'); ?>
-<?php $__env->startSection('action', 'Controller pannel'); ?>
-<?php $__env->startSection('content'); ?>
-    <div class="page-content">this is my home page</div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('admins.layouts.master', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
